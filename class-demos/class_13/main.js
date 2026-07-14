ee7f87c0c9f290a5e1e1470d4bb77730d149e08f
Kayla Celestin <kaylacelestin@Kaylas-MacBook-Pro.local>
2026-07-14 16:26:38 -0400

    project3 upload

--- class-demos/class_13/main.js
+++ class-demos/class_13/main.js
@@ -1,18 +1,18 @@
 window.onload = () =>{
 	//1st param: function name tht is called after milliseconds passed fr
 	// 2nd param: millisec. before function is executed fr
-setTimeout(sayWassup, 3000)
+	setTimeout(sayWassup, 3000)
 
 	// this mf timeout will not happen bc it will be cleared fr
-let mytimer = setTimeout(yo, 2000)
-console.log(mytimer) // printing out the id of the mf timerrrr
-clearTimeout(mytimer) // removing the timerrrrrr
+	let mytimer = setTimeout(yo, 2000)
+	console.log(mytimer) // printing out the id of the mf timerrrr
+	clearTimeout(mytimer) // removing the timerrrrrr
 
 
 	// add a mf timer tht repeats every 3 sec
-	let myInterval = setInterval(myInterval,3000)
+	setInterval(myInterval,3000)
 
-	document.body.addEventListner("click", handleClick)
+	document.body.addEventListener("click", handleClick)
 
 }
 
@@ -21,19 +21,19 @@ clearTimeout(mytimer) // removing the timerrrrrr
 // we gotta define sayWassup
 // function is jst like let but 2 make a function instead of variable fr
 function sayWassup(){
-	console.log('waasup')
-	document.getElementById('conetnt').textContent = "wassup"
+	console.log('wassup')
+	document.getElementById('content').textContent = "wassup"
 }
 
 // this mf another function decleration
-function hi(){
+function yo(){
 	console.log('yo')
 }
 
 function myInterval(){
 	console.log('3 seconds')
 	let newElement = document.createElement('span')
-	newElement = "yoo "
+	newElement.textContent = "yoo "
 	document.querySelector("#content").appendChild(newElement)
 }
 function handleClick(){
---
+++ project3/proposal.md
@@ -0,0 +1,15 @@
+* A SPLIFF BURNING AND ASHING --> CALCULATING THE TIME TO SMOKE AGAIN
+MAYBE EYES ( AFTER PUFFS ) IN BACKGROUND
+
+* time lapse ( in multiple pictures ) of spliff burning , when spliff done maybe message " TIME 2 ROLL "
+
+* THE UNIT OF TIME IS SMOKE ; THE SPLIFF BURNS TO THE FILTER , THEN IT IS TIME 2 ROLL UP AGAIN !
+
+- [INSPIRATION FR](https://www.instagram.com/p/Ck_h0nRuXl-/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==)
+
+- [MORE INSPIRATION FR] :
+![alt text](proposal_images/Wish_time_never_mattered.jpg)
+
+-[just a little more heehee] (https://publicrecords.nyc)
+
+- ROUGH SKETCH FR ![alt text](proposal_images/PROJECT_3_PROPOSAL%20copy.png)
